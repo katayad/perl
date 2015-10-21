e12b8b007554323b06d248bebcdfe9955f1ffec9
KatayaD <kataya97@gmail.com>
2015-10-21 16:33:17 +0300

    3d task update

--- 3/Local/PerlCourse/GetterSetter.pm
+++ 3/Local/PerlCourse/GetterSetter.pm
@@ -7,26 +7,26 @@ $\ = "\n";
 		$Local::SomePackage::test = sub {return 0;};
 
 		$pack = caller;
-		*{"$pack::FGBVHDNb"};
-	    for $var (@_[1..$#_]) {
+		*{"$pack::GlobalModuleVar"};
+		for $var (@_[1..$#_]) {
 			no strict 'refs';
 
-			${"$pack::FGBVHDNb"} = "$pack::$var";
-	        $f = $pack."::set_".$var;
+			${"$pack::GlobalModuleVar"} = "$pack::$var";
+			$f = $pack."::set_".$var;
 
-	       	*{$f} = sub {
-	       		state $v = ${"$pack::FGBVHDNb"};
-	        	${$v} = @_[0];
-	        };
-	        $f->(${"$pack::$var"});
+			*{$f} = sub {
+				state $v = ${"$pack::GlobalModuleVar"};
+				${$v} = @_[0];
+			};
+			$f->(${"$pack::$var"});
 
-	        $f = $pack."::get_".$var;
-	       	*{$f} = sub {
-	       		state $v = ${"$pack::FGBVHDNb"};
-	       		#print $v;
-	       		return ${$v};
-	        };
-	        $f->();
-	    }
+			$f = $pack."::get_".$var;
+			*{$f} = sub {
+				state $v = ${"$pack::GlobalModuleVar"};
+				#print $v;
+				return ${$v};
+			};
+			$f->();
+		}
 	}
 1;
